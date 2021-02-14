fef4edf0fa02299136bbbb8dc4b55e3c48c9db79
Roddy Munro <roddymunro@icloud.com>
2021-02-13 20:32:13 -0400

    Initial Commit

---
+++ NewFeaturesSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewFeaturesSwiftUI
+//
+//  Created by Roddy Munro on 13/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewFeaturesSwiftUI/NewFeaturesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewFeaturesSwiftUIApp.swift
+//  NewFeaturesSwiftUI
+//
+//  Created by Roddy Munro on 13/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewFeaturesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
